03c5ed75fd11be127cfe4c3cd186eae48eca4142
Neurum <fullstackwebdev78@gmail.com>
2023-01-13 13:25:16 -0500

    Place files in to folders for better organization

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,19 +5,22 @@ author 'Knight'
 description 'Parking Enforcement Job'
 version '1.0.0'
 
-files {
-    'sounds/receipt_print.mp3',
-    'sounds/receipt_tear.mp3'
-}
 
 -- What to run
 client_scripts {
-    'client.lua',
-    'config.lua',
-    'dutyStations.lua',
-    'functions.lua'
+    'client/client.lua',
+    'client/config.lua',
+    'client/dutyStations.lua',
+    'client/functions.lua'
 }
 server_scripts {
-    'server.lua',
+    'server/server.lua',
     
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html'
+    'html/sounds/ticket.ogg',
 }
